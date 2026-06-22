eb67e137be9c45a9a8a603469d8dcdc9c2ca40c1
Rafifs-ops <rafifebrians150206@gmail.com>
2026-06-22 08:06:15 +0700

    fix nodemailer 4

--- server/api/send-otp.post.ts
+++ server/api/send-otp.post.ts
@@ -38,10 +38,8 @@ export default defineEventHandler(async (event) => {
     // Kirim email
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
-        port: 587,
-        secure: false, // upgrade later with STARTTLS
-        requireTLS: true,
-        family: 4, // Force IPv4 karena server mungkin tidak support IPv6
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
