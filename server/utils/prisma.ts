745a9a1dc8af0ee16e5fc28c17001dd5b326c46b
Rafifs-ops <rafifebrians150206@gmail.com>
2026-07-24 02:35:28 +0700

    fix prisma 2

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -1,18 +1,18 @@
 import { PrismaClient } from '@prisma/client'
 import { createClient } from '@libsql/client'
-import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { PrismaLibSql } from '@prisma/adapter-libsql'
 
 
 const config = useRuntimeConfig()
 
 // Inisialisasi koneksi libSQL (Turso)
-const libsql = createClient({
+const libsql: any = createClient({
     url: config.tursoDatabaseUrl,
     authToken: config.tursoAuthToken,
 })
 
 // Pasang adapter ke Prisma
-const adapter = new PrismaLibSQL(libsql)
+const adapter = new PrismaLibSql(libsql)
 
 // Export instance Prisma agar bisa digunakan di seluruh file API Nuxt
 export const prisma = new PrismaClient({ adapter })
